28bb579f42c133808b5f363b6bc1b705fa6f4227
Vinicius Reis <luiz.vinicius73@gmail.com>
2015-10-23 17:13:47 -0200

    single page app
    todo o sistema agora funciona como uma single page, facilitando a
    criação de seções

--- vendor/js/app.js
+++ vendor/js/app.js
@@ -1,35 +1,76 @@
-(function(){
-	angular.module('awesome', ['iso.directives'])
-	.directive('widget', function() {
-	  return {
-	      restrict: 'C',
-	      link: function(scope, elem, attrs) {
-	      	$(elem).geopattern(scope.item.name, {
-	      		generator: 'squares'
-	      	});
-	      }
-	  };
-	})
-	.controller('filterFormController', ['$scope', '$http', '$timeout', function($scope, $http, $timeout){
-		var init = function() {
-			$http.get('itens.json')
-				.then(function(_response){
-					$scope.itens = _response.data;
-				});
-		};
+(function() {
+  angular.module('awesome', ['iso.directives', 'ngRoute'])
+    .directive('widget', function() {
+      return {
+        restrict: 'C',
+        link: function(scope, elem, attrs) {
+          $(elem).geopattern(scope.item.name, {
+            generator: 'squares'
+          });
+        }
+      };
+    })
 
-		$scope.itens = [];
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/home.html',
+        controller: 'homeController'
+      })
+			.when('/:sectionName', {
+        templateUrl: 'views/section.html',
+        controller: 'sectionController'
+      });
+  }])
 
-		$scope.setFilter = function(_value){
-			$scope.query = _value;
-		}
+  .run(['$rootScope', function($rootScope) {
+    $rootScope.$on('$routeChangeSuccess', function(scope, data) {
+      $rootScope.currentController = data.controller;
+    });
+  }])
 
-		 $scope.$watch('query', function() {
-		 	$timeout(function(){
-		 		$scope.$emit('iso-method', {name:'arrange', params:null})
-		 	}, 600);
-		});
+  .controller('mainController', ['$scope', '$http', '$timeout', function($scope, $http, $timeout) {
 
-		init();
-	}]);
+  }])
+
+	.controller('homeController', ['$scope', '$http', '$timeout', function($scope, $http, $timeout) {
+    var init = function() {
+      $http.get('sections.json')
+        .then(function(_response) {
+          $scope.itens = _response.data;
+        });
+    };
+
+    $scope.itens = [];
+
+    init();
+  }])
+
+  .controller('sectionController', ['$scope', '$http', '$timeout', '$routeParams', function($scope, $http, $timeout, $routeParams) {
+    var init = function() {
+      var file = 'sections/' + $routeParams.sectionName + '.json';
+
+      $http.get(file)
+        .then(function(_response) {
+          $scope.itens = _response.data;
+        });
+    };
+
+    $scope.itens = [];
+
+    $scope.setFilter = function(_value) {
+      $scope.query = _value;
+    }
+
+    $scope.$watch('query', function() {
+      $timeout(function() {
+        $scope.$emit('iso-method', {
+          name: 'arrange',
+          params: null
+        })
+      }, 600);
+    });
+
+    init();
+  }]);
 })();
