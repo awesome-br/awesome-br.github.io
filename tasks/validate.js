a211091784d10b50ad814c1d86cdba0857a702b9
Daniel Schmitz <danieljfa@gmail.com>
2015-12-13 13:43:48 -0200

    Os tipos permitidos foram extraídos do arquivo de validação para a raiz do projeto

--- tasks/validate.js
+++ tasks/validate.js
@@ -1,45 +1,20 @@
 var jlint = require('jlint');
 var log = require('pretty-log');
 var _ = require('lodash');
+var types = require('../types');
 
 var options = {
   pattern: './sections/*.json'
 };
-
-/**
- * Tipos de link válidos
- * Isso evita de termos artigo e artigos, por exemplo.
- */ 
-var validTypes = [
-  "artigo",
-  "livro",
-  "video",
-  "screencast",
-  "styleguide",
-  "palestra",
-  "tutorial",
-  "dica",
-  "slide",
-  "vídeo",
-  "documentação",
-  "url",
-  "curso",
-  "guia",
-  "pacote",
-  "hangout",
-  "slideshare",
-  "apostila",
-  "site"
-  ];
 
 jlint(options, function(error, json, file) {
   
   for (var index in json){
     var type = json[index].type;
     if (file=="./sections/base.json") continue; //base pode pular.
-    if (validTypes.indexOf(type)==-1){
+    if (types.validTypes.indexOf(type)==-1){
       log.error(file);
-      console.log("Tipo inválido para o link: \"" + type + "\". Se você acredita que este é um tipo de link válido, por favor abra uma ISSUE requisitando o mesmo. Os tipos cadastrados são: " + JSON.stringify(validTypes)  );
+      console.log("Tipo inválido para o link: \"" + type + "\". Se você acredita que este é um tipo de link válido, por favor abra uma ISSUE requisitando o mesmo. Os tipos cadastrados são: " + JSON.stringify(types.validTypes)  );
       process.exit(1);
     }
   }
