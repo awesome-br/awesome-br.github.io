c93ffe99373e3917579752f135f02206a752a544
danielschmitz <danieljfa@gmail.com>
2015-12-13 09:36:27 -0200

    Nova validação incluindo tipos de link, que devem obedecer a uma lista

--- tasks/validate.js
+++ tasks/validate.js
@@ -6,8 +6,43 @@ var options = {
   pattern: './sections/*.json'
 };
 
-jlint(options, function(error, json, file) {
+/**
+ * Tipos de link válidos
+ * Isso evita de termos artigo e artigos, por exemplo.
+ */ 
+var validTypes = [
+  "artigo",
+  "livro",
+  "video",
+  "screencast",
+  "styleguide",
+  "palestra",
+  "tutorial",
+  "dica",
+  "slide",
+  "vídeo",
+  "documentação",
+  "url",
+  "curso",
+  "guia",
+  "pacote",
+  "hangout",
+  "slideshare",
+  "apostila",
+  "site"
+  ];
 
+jlint(options, function(error, json, file) {
+  
+  for (var index in json){
+    var type = json[index].type;
+    if (file=="./sections/base.json") continue; //base pode pular.
+    if (validTypes.indexOf(type)==-1){
+      log.error(file);
+      console.log("Tipo inválido para o link: \"" + type + "\". Se você acredita que este é um tipo de link válido, por favor abra uma ISSUE requisitando o mesmo. Os tipos cadastrados são: " + JSON.stringify(validTypes)  );
+      process.exit(1);
+    }
+  }
   if (!_.isEmpty(error)) {
     log.error(file);
     console.log(error.message);
