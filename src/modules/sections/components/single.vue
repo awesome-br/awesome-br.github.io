8c7b4cb5efdbb84385efbb2a610c6f2c88a855ef
Vinicius Reis <luiz.vinicius73@gmail.com>
2016-10-12 05:18:35 -0300

    list links

--- src/modules/sections/components/single.vue
+++ src/modules/sections/components/single.vue
@@ -1,5 +1,20 @@
 <template>
-  <h1>{{ section.name }}</h1>
+  <div class="container">
+    <div class="row">
+      <header class="page-header col-md-12">
+        <h1>{{ section.name }}</h1>
+      </header>
+    </div>
+
+    <div class="row">
+      <div class="col-xs-12 col-sm-4 col-md-4 col-lg-3" v-for="link in links">
+        <a :href="link.url" target="_new" class="item" v-geopattern="link.name">
+          <span class="badge">{{ link.type }}</span>
+          <h3>{{ link.name }}</h3>
+        </a>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -17,7 +32,21 @@ export default {
     ...mapActions(['setCurrent', 'cleanCurrent'])
   },
   computed: {
-    ...mapGetters({section: 'sectionData'})
+    ...mapGetters({section: 'sectionData'}),
+    links () {
+      return this.$store.state.sections.links
+    }
   }
 }
 </script>
+
+<style scoped>
+  .item {
+    display: block;
+    color: #fff;
+    margin: 10px 0;
+    padding: 10px;
+    min-height: 100px;
+    background-color: #eee;
+  }
+</style>
