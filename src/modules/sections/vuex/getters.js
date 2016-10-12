9381c190f4445125e569b290b38cb9aa2f805aca
Vinicius Reis <luiz.vinicius73@gmail.com>
2016-10-12 05:11:16 -0300

    prevent undefined

--- src/modules/sections/vuex/getters.js
+++ src/modules/sections/vuex/getters.js
@@ -1,2 +1,2 @@
 export const hasList = ({ list }) => list.length > 0
-export const sectionData = ({ current, list }) => list.find(x => x.url === current)
+export const sectionData = ({ current, list }) => list.find(x => x.url === current) || {}
